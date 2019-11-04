fa07ea7ca111598d702b520c2144fa0bf8e0ce54
shadow88sky <119136016@qq.com>
2019-11-04 11:29:48 +0800

    hello world

---
+++ helloworld/hello.scala
@@ -0,0 +1,9 @@
+// object 表示一个伴生对象，这里我们可以简单的理解就是一个对象
+// HelloWorld 就是对象名字，它底层真正对应的类名是HelloWorld$,对象是HelloWorld$类型的一个静态对象MODULE$
+// 当我们编写一个object HelloWorld底层会生成两个.class文件，分别是HelloWorld 和HelloWorld$
+object HelloWorld
+{
+    def main(args: Array[String]): Unit = {
+        println("Hello World");
+    }
+}
